31c9d10f9150be690e34aae376f0f35a16c2b69c
tsanming <tsanmmmm2000@hotmail.com>
2018-05-23 14:13:43 +0800

    finish

--- dd.c
+++ dd.c
@@ -34,37 +34,37 @@ int main() {
         scanf("%s", &operation);
         switch (operation) {
             case 'M': {
-		        int node_number = 0;
-		        scanf("%d", &node_number);
-			    make_tree(node_number);
-	            break;
+                int node_number = 0;
+                scanf("%d", &node_number);
+                make_tree(node_number);
+                break;
             }
-		    case 'G': {
-			    int child_number = 0;
+            case 'G': {
+                int child_number = 0;
                 int parent_number = 0;
                 scanf("%d %d", &child_number, &parent_number);
                 graft(child_number, parent_number);	
-		        break;
+                break;
             }
-		    case 'F': {
-		        int node_number = 0;
-		        scanf("%d", &node_number);
+            case 'F': {
+                int node_number = 0;
+                scanf("%d", &node_number);
                 int depth = find_depth(node_number, 0);
                 struct dd_result result;
                 result.number = node_number;
                 result.depth = depth;
                 results[count] = result;
                 count++;
-		        break;
+                break;
             }
-		    default: {
-			   for(int i = 0; i < count; i++) {
-				   struct dd_result result = results[i];
-				   printf("%d %d\n", result.number, result.depth);
-			   }
-			   stop = 1;
-		       break;
-		    }
-	    }
-	}
+            default: {
+                for(int i = 0; i < count; i++) {
+                   struct dd_result result = results[i];
+                   printf("%d %d\n", result.number, result.depth);
+                }
+                stop = 1;
+                break;
+            }
+        }
+    }
 }
